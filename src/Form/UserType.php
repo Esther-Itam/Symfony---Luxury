23c8a34118e0e35741b3b1439479ff914e1390ed
Esther Itam <esther.itam@live.fr>
2020-07-22 11:38:28 +0200

    crud Job offer et mise en palce des templates new et show

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,13 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
@@ -14,7 +21,13 @@ class UserType extends AbstractType
         $builder
             ->add('email')
             ->add('password')
-            ->add('gender')
+            ->add('gender', ChoiceType::class,[
+                'choices' => [
+                    'female' => 'female',
+                    'male' => 'male',
+                    'transgender' => 'transgender',
+                ],
+            ])
             ->add('firstName')
             ->add('lastName')
             ->add('phoneNumber')
@@ -25,16 +38,88 @@ class UserType extends AbstractType
             ->add('nationality')
             ->add('birthDate')
             ->add('birthPlace')
-            ->add('passport')
-            ->add('resume')
-            ->add('experience')
-            ->add('description')
+            ->add('passport', CheckboxType::class,  array('required' => false))
+            ->add('passport_file', FileType::class, [
+                'label' => 'Passport (pdf file)',
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
+            ])
+            ->add('resume', FileType::class, [
+                'label' => 'CV (pdf file)',
+                 // unmapped means that this field is not associated to any entity property
+                 'mapped' => false,
+
+                 // make it optional so you don't have to re-upload the PDF file
+                 // every time you edit the Product details
+                 'required' => false,
+ 
+                 // unmapped fields can't define their validation using annotations
+                 // in the associated entity, so you can use the PHP constraint classes
+                 'constraints' => [
+                     new File([
+                         'maxSize' => '1024k',
+                         'mimeTypes' => [
+                             'application/pdf',
+                             'application/x-pdf',
+                         ],
+                         'mimeTypesMessage' => 'Please upload a valid PDF document',
+                     ])
+                 ],
+            ])
+            ->add('experience', ChoiceType::class,[
+                'choices' => [
+                    '0-6 months' => '0-6 months',
+                    '6 months - 1 year' => '6 months - 1 year',
+                    '1-2 years' => '1-2 years',
+                    '2+ years' => '2+ years',
+                    '5+ years' => '5+ years',
+                    '10+ years' => '10+ years',
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'row_attr' => ['class' => 'text-editor', 'id' => '...'],
+                'attr' => ['class' => 'tinymce'],])
             ->add('note')
-            ->add('createdAt')
-            ->add('updatedAt')
+            ->add('createdAt', DateType::class, [
+                'widget' => 'single_text',
+                'input'  => 'datetime_immutable',
+                'required'   => false,
+            ])
+            ->add('updatedAt', DateType::class, [
+                'widget' => 'single_text',
+                'input'  => 'datetime_immutable',
+                'required'   => false,
+            ])
             ->add('isAdmin')
             ->add('availability')
-            ->add('jobCategory')
+            ->add('jobCategory', ChoiceType::class,[
+                'choices' => [
+                    'Commercial' => 'Commercial',
+                    'Retail sales' => 'Retail sales',
+                    'Creative' => 'Creative',
+                    'Technology' => 'Technology',
+                    'Marketing & PR' => 'Marketing & PR',
+                    'Fashion & luxury' => 'Fashion & luxury',
+                    'Management & HR' => 'Management & HR'
+                ],
+            ])
         ;
     }
 
